6fc6536d4a518e173bcbf52f110a45f94b3142ab
Khris H. <contact@devkhris.com>
2026-09-12 17:14:36 -0600

    feat: base health api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import "reflect-metadata"
 import express, { Application } from "express"
 import cookieParser from "cookie-parser"
 import logger from "morgan"
-import { AppDataSource } from "./data-source"
-import * as dotenv from "dotenv"
-
-dotenv.config()
 
 const app: Application = express()
 
@@ -14,13 +9,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+app.get("/health", (_req, res) => {
+    res.json({ message: "API is running" })
+})
+
 const PORT = process.env.PORT || 3000
 
-AppDataSource.initialize().then(async () => {
-    console.log("Database connected")
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
-}).catch(error => console.log(error))
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
 export default app
