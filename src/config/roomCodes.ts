e2f05bd22f8e84092229c6c6c9466639e5ac616a
Khris H. <contact@devkhris.com>
2026-09-14 02:10:09 -0600

    feat: deleted room codes util

--- src/config/roomCodes.ts
+++
@@ -1,9 +0,0 @@
-const VALID_ROOM_CODES = ['TRAINING01', 'TRAINING02', 'DEMO001']
-
-export function isValidRoomCode(code: string): boolean {
-  return VALID_ROOM_CODES.includes(code.toUpperCase())
-}
-
-export function getValidRoomCodes(): string[] {
-  return [...VALID_ROOM_CODES]
-}
